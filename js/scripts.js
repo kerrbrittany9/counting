d6c4d2745460cd3683d4b3c37babe97916be26df
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-08 20:47:37 -0700

    create form and write basic js variables

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
   $("submitButton").click(function(event) {
     event.preventDefault();
     var submitNumber = $("prompt").val();
+    var countByNumber = $("countBy").val();
     var newNumber = "";
 
-    for (var index = 0; index < prompt.length; index += 1) {
+    for (var index = 0; index < prompt.length; index += 1[countBy]) {
 
       $("#result").append("<h2>" + newText + "</h2>");
     }
