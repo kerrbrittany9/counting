e3e8b61c8e055c0bf8447d6d88a07f490da4e1c9
kerrbrittany9@gmail.com <brittanykerr@brittany-kerrs-macbook-pro.local>
2017-06-08 20:43:51 -0700

    write html for form and define variables for loop

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("submitButton").click(function(event) {
+    event.preventDefault();
+    var submitNumber = $("prompt").val();
+    var newNumber = "";
+
+    for (var index = 0; index < prompt.length; index += 1) {
+
+      $("#result").append("<h2>" + newText + "</h2>");
+    }
+  }
+}
